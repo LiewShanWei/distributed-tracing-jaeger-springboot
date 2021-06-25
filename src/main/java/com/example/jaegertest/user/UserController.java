e6cd93be8087d389b01948804f334a882e098dae
LiewShanWei <shanweiliew93@gmail.com>
2021-06-25 17:51:56 +0800

    Added jaeger springboot tracing

--- src/main/java/com/example/jaegertest/user/UserController.java
+++ src/main/java/com/example/jaegertest/user/UserController.java
@@ -1,21 +1,30 @@
 package com.example.jaegertest.user;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private Tracer tracer;
 
     @GetMapping(value = "/user/test", produces = MediaType.TEXT_PLAIN_VALUE )
     public String getTestStringApi(){
-        return "Program is running";
+        Span span = tracer.activeSpan();
+        String s = "Program is running";
+        span.log(Map.of("event","declare s"));
+
+        span.finish();
+        return s;
     }
 
     @GetMapping(value = "/user/get/{id}")
--- src/main/java/com/example/jaegertest/user/UserService.java
+++ src/main/java/com/example/jaegertest/user/UserService.java
@@ -1,10 +1,21 @@
 package com.example.jaegertest.user;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class UserService {
+    @Autowired
+    private Tracer tracer;
+
     public User getUser(String id){
+        Span span = tracer.buildSpan("get-user-service").start();
+        span.log(Map.of("event","Id: " + id));
+        span.finish();
         return new User(id,"Test");
     }
 }
