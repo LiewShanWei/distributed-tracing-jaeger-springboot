4139e6bfd6122ba8131ceacedb4c6a99d470d4fd
LiewShanWei <shanweiliew93@gmail.com>
2021-07-02 12:18:56 +0800

    Added RestTemplate bean Added api to call another microservice

--- src/main/java/com/example/jaegertest/user/UserController.java
+++ src/main/java/com/example/jaegertest/user/UserController.java
@@ -5,11 +5,14 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping(value = "/user/test", produces = MediaType.TEXT_PLAIN_VALUE )
     public String getTestStringApi(){
@@ -19,5 +22,14 @@ public class UserController {
     @GetMapping(value = "/user/get/{id}")
     public User getUserApi(@PathVariable String id){
         return userService.getUser(id);
+    }
+
+    @GetMapping(value = "/user2/get/{id}")
+    public User getUserFromAnotherApi(@PathVariable String id){
+        final String uri = "http://localhost:8082/user/get/" + id;
+        RestTemplate restTemplate = new RestTemplate();
+
+        User result = restTemplate.getForObject(uri, User.class);
+        return result;
     }
 }
